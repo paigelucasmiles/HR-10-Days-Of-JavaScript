f6bab1ff2668bc43bb08c0694fd80c81d865ec85
Paige Miles <paige@lucas-miles.com>
2020-12-31 10:32:42 -0700

    day1 completed

---
+++ day1.js
@@ -0,0 +1,18 @@
+// Day 1: Arithmetic Operators
+
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    console.log(area);
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = (length + width) * 2
+    console.log(perimeter);
+}
+
+getArea(10, 2)
+getPerimeter(10, 2)
