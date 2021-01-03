0b001ff97a1bb08629438eadd49531d806499306
Paige Miles <paige@lucas-miles.com>
2021-01-02 17:41:01 -0700

    day2: conditional statements: switch. completed

--- day2.js
+++ day2.js
@@ -63,3 +63,48 @@ function getGrade(score) {
 
 // Output Format
 // Return either A, B, C, or D according to the criteria given above.
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    let first = s[0]
+    switch (first){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = "A";
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = "B"
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = "C"
+            break;
+        case 'n':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            letter = "D"
+            break;
+    }
+    console.log(letter);
+}
+
+getLetter(["k", "b"])
