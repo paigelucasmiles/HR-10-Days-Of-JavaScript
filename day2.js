36769317bd981c85968f15ffaf9798a407254155
Paige Miles <paige@lucas-miles.com>
2021-01-06 08:16:19 -0700

    day2: loops - completed

--- day2.js
+++ day2.js
@@ -107,4 +107,33 @@ function getLetter(s) {
     console.log(letter);
 }
 
-getLetter(["k", "b"])
+
+// Day 2: Loops
+
+// Objective
+// In this challenge, we practice looping over the characters of string. Check out the attached tutorial for more details.
+
+// Task
+// Complete the vowelsAndConsonants function in the editor below. It has one parameter, a string, s , consisting of lowercase English alphabetic letters (i.e., a through z). The function must do the following:
+// 1. First, print each vowel in s on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in s.
+// 2. Second, print each consonant (i.e., non-vowel) in s on a new line in the same order as it appeared in s.
+
+// Input Format
+// Locked stub code in the editor reads string s from stdin and passes it to the function.
+
+// Output Format
+// First, print each vowel in s on a new line (in the same order as they appeared in s). Second, print each consonant (i.e., non-vowel) in s on a new line (in the same order as they appeared in s).
+
+function vowelsAndConsonants(s) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    const stringAsArray = s.split("");
+
+    const vowelsFromString = stringAsArray.filter( element => vowels.includes(element));
+
+    const consontantsFromString = stringAsArray.filter( element => !vowels.includes(element));
+
+    console.log(vowelsFromString.join('\n') + '\n' + consontantsFromString.join('\n'))
+}
+
+vowelsAndConsonants("javascript")
