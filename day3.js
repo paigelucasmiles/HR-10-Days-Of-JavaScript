ba5b45b8fefde0fd2af15a46d227885ffb8c908c
Paige Miles <paige@lucas-miles.com>
2021-01-06 09:41:11 -0700

    added Day3: arrays problem

---
+++ day3.js
@@ -0,0 +1,24 @@
+// Day 3: Arrays
+
+// Objective
+// In this challenge, we learn about Arrays. Check out the attached tutorial for more details.
+
+// Task
+// Complete the getSecondLargest function in the editor below. It has one parameter: an array, NUMS, of N numbers. The function must find and return the second largest number in NUMS.
+
+// Input Format
+// Locked stub code in the editor reads the following input from stdin and passes it to the function:
+// The first line contains an integer, N , denoting the size of the NUMS array.
+// The second line contains N space-separated numbers describing the elements in NUMS.
+
+// Constraints
+// 1 <= n <= 10
+// 0 <= NUMS[i] <= 100, where NUMS[i] is the number at index i.
+// The numbers in NUMS are not distinct.
+
+// Output Format
+// Return the value of the second largest number in the NUMS array.
+
+function getSecondLargest(nums) {
+    // Complete the function
+}
