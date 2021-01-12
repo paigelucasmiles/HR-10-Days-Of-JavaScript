a111c98ab8f14b76f1441d155c1a270a42a2e7f4
Paige Miles <paige@lucas-miles.com>
2021-01-12 16:25:31 -0700

    completed try, catch, finally problem

--- day3.js
+++ day3.js
@@ -34,3 +34,36 @@ function getSecondLargest(nums) {
 }
 
 getSecondLargest([2, 7, 3, 7])
+
+// Objective
+// In this challenge, we learn about strings and exceptions. Check out the attached tutorials for more details.
+
+// Task
+// Complete the reverseString function; it has one parameter, s. You must perform the following actions:
+// 1. Try to reverse string s using the split, reverse, and join methods.
+// 2. If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+// 3. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+// Input Format
+// Locked stub code in the editor reads variable s from stdin and passes it to the function.
+
+// Output Format
+// You must write two print statements using console.log():
+// 1. Print s the contents of a caught exception's message on a new line. If no exception was thrown, this line should not be printed.
+// 2. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+const s = '1234'
+const t = Number(1234)
+
+function reverseString(s) {
+    try {
+        s = s.split("").reverse().join("")
+    } catch (e){
+        console.log(e.message)
+    } finally {
+        console.log(s)
+    }
+}
+
+reverseString(t)
+
