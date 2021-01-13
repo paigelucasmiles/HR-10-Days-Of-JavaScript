18be337f80fa4747d2c0a36497f64f0ced1e16db
Paige Miles <paige@lucas-miles.com>
2021-01-13 10:44:44 -0700

    day4: count objects. Completed

--- day4.js
+++ day4.js
@@ -21,7 +21,7 @@
 // Output Format
 // Return a object that has the properties specified above. Locked code in the editor prints the returned object's length, width, perimeter, and area to STDOUT.
 
-function Rectangle(a, b){
+function rectangle(a, b){
     console.log({
         length: a,
         width:  b,
@@ -30,4 +30,40 @@ function Rectangle(a, b){
     })
 }
 
-Rectangle(4, 5)
+rectangle(4, 5)
+
+// Day 4: Count Objects
+
+// Objective
+// In this challenge, we learn about iterating over objects. Check the attached tutorial for more details.
+
+// Task
+// Complete the function in the editor. It has one parameter: an array, a, of objects. Each object in the array has two integer properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x == o.y.
+
+// Constraints
+// 5 <= n <= 10
+// 1 <= x, y <= 100
+
+// Output Format
+// Return a count of the total number of objects o such that o.x == o.y. Locked stub code in the editor prints the returned value to STDOUT.
+
+array = [
+    {x: 1, y: 1},
+    {x: 2, y: 3},
+    {x: 3, y: 3},
+    {x: 3, y: 4},
+    {x: 4, y: 5}
+]
+
+function getCount(objects) {
+    let o = 0
+
+    for (let i = 0; i < objects.length; i++) {
+        if (objects[i].x === objects[i].y) {
+            o++
+        }
+    }
+    console.log(o)
+}
+
+getCount(array)
